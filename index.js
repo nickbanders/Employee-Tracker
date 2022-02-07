71409867519692ac5439939ece3004bc3b75828e
nickbanders <nickbanders@gmail.com>
2022-02-07 03:57:35 -0600

    finished add employee function

--- index.js
+++ index.js
@@ -24,14 +24,14 @@ const selectDept = () => {
 
 const selectRole = () => {
   roleSelect = [];
-  let role = `SELECT * FROM roles`;
+  let role = `SELECT title FROM roles`;
   db.query(role, (err, rows) => {
     if (err) {
       console.log(err);
       return err;
     } else {
       for (var i = 0; i < rows.length; i++) {
-        var roleList = rows[i].name;
+        var roleList = rows[i].title;
         roleSelect.push(roleList);
       }
     }
@@ -56,7 +56,7 @@ const selectMgr = () => {
 
 const selectEmploy = () => {
   employSelect = [];
-  let emp = `SELECT CONCAT(first_name, ' ', last_name) AS name FROM employee ORDER BY id`;
+  let emp = `SELECT CONCAT(first_name, ' ', last_name) AS name FROM employee ORDER BY last_name`;
   db.query(emp, (err, rows) => {
     if (err) {
       console.log(err);
@@ -71,10 +71,10 @@ const selectEmploy = () => {
 };
 
 const promptUser = () => {
-  selectDept();
+  selectEmploy();
   selectRole();
   selectMgr();
-  selectEmploy();
+  selectDept();
   return inquirer
     .prompt([
       {
@@ -88,7 +88,7 @@ const promptUser = () => {
           "Add a Department",
           "Add a Role",
           "Add an Employee",
-          "Update an Employee Role",
+          "Update an Employee's Role",
           "Exit",
         ],
       },
@@ -114,7 +114,7 @@ const promptUser = () => {
           promptEmployee();
           break;
         case "update an Employee Role":
-          updateRole();
+          updEmp();
           break;
         case "Exit":
           console.log("Have a nice day!");
@@ -147,7 +147,7 @@ const viewRoles = () => {
 };
 
 const viewEmployee = () => {
-  const sql = `SELECT e.id, e.first_name, e.last_name, r.title, r.salary, e.manager_id, d.dept_name FROM employee AS e JOIN roles AS r ON r.id = e.role_id JOIN department AS d ON d.id = r.department_id ORDER BY d.dept_name`;
+  const sql = `SELECT e.id, e.first_name, e.last_name, r.title, r.salary, e.manager_id, d.dept_name FROM employee AS e JOIN roles AS r ON r.id = e.role_id JOIN department AS d ON d.id = r.department_id ORDER BY e.id`;
   db.query(sql, (err, rows) => {
     if (err) {
       return err;
@@ -222,68 +222,129 @@ const promptEmployee = () => {
       {
         type: "input",
         name: "first_name",
-        message: "What is the employee's first name?",
+        message: "Enter employee's first name:",
       },
       {
         type: "input",
         name: "last_name",
-        message: "What is the employee's last name?",
+        message: "Enter employee's last name:",
       },
-      // {
-      //   type: "list",
-      //   name: "department_id",
-      //   message: "What is their role?",
-      //   choices: deptSelect,
-      // },
-      // {
-      //   type: "list",
-      //   name: "role_id",
-      //   message: "What is their role?",
-      //   choices: roleSelect,
-      // },
+      {
+        type: "list",
+        name: "role_id",
+        message: "Select employee role:",
+        choices: roleSelect,
+      },
       {
         type: "list",
         name: "manager_id",
-        message: "Who is their manager?",
+        message: "Who is the employee's manager?",
         choices: mgrSelect,
       },
-      // {
-      //   type: "confirm",
-      //   name: "is_manager",
-      //   message: "Is this person a manager?",
-      //   default: false,
-      // },
+      {
+        type: "confirm",
+        name: "is_manager",
+        message: "Is this person a manager?",
+        default: false,
+      },
     ])
     .then((answer) => {
-      let deptPick;
-      for (let i = 0; i < deptSelect.length; i++) {
-        if (deptSelect[i] === answer.department_id) {
-          deptPick = i + 1;
+      let rolePick;
+      for (var i = 0; i < roleSelect.length; i++) {
+        if (roleSelect[i] === answer.role_id) {
+          rolePick = i + 1;
         }
       }
-      // let rolePick;
-      // for (let i = 0; i < roleSelect.length; i++) {
-      //   if (roleSelect[i] === answer.role_id) {
-      //     rolePick = i + 1;
-      //   }
-      // }
       let mgrPick;
-      for (let i = 0; i < mgrSelect.length; i++) {
+      for (var i = 0; i < mgrSelect.length; i++) {
         if (mgrSelect[i] === answer.manager_id) {
           mgrPick = i + 1;
         }
       }
-      // let mgrPickSelection;
-      // if (answer.is_manager == false) {
-      //   mgrPickSelection = 0;
-      // } else {
-      //   mgrPickSelection = 1;
-      // }
-      const sql = `INSERT INTO employee (first_name, last_name, manager_id) 
-      VALUES ('${answer.first_name}', '${answer.last_name}', '${mgrPick}')`;
+      let isMgr;
+      if (answer.is_manager == false) {
+        isMgr = 0;
+      } else {
+        isMgr = 1;
+      }
 
-      // const sql = `INSERT INTO employee (first_name, last_name, role_id, is_manager, manager_id) VALUES ('${answer.first_name}', '${answer.last_name}', '${chosenRole}', '${chosenIs_manager}', '${chosenMgr}')`;
+      const sql = `INSERT INTO employee (first_name, last_name, role_id, is_manager, manager_id) 
+      VALUES ('${answer.first_name}', '${answer.last_name}', '${rolePick}', '${isMgr}', '${mgrPick}')`;
+      db.query(sql, (err, rows) => {
+        if (err) {
+          console.log(err);
+          return err;
+        }
+        promptUser();
+      });
+    });
+};
 
+const updEmp = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "id",
+        message: "Choose employee for update",
+        choices: employSelect,
+      },
+      {
+        type: "input",
+        name: "first_name",
+        message: "Enter employee's first name",
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "enter employee's last name",
+      },
+      {
+        type: "list",
+        name: "role_id",
+        message: "Select employee's role",
+        choices: roleSelect,
+      },
+      {
+        type: "list",
+        name: "manager_id",
+        message: "Who is the employee's manager?",
+        choices: mgrSelect,
+      },
+      {
+        type: "confirm",
+        name: "is_manager",
+        message: "Is this employee a manager?",
+        default: false,
+      },
+    ])
+    .then((answer) => {
+      let empChoice;
+      for (var i = 0; 1 < employSelect.length; i++) {
+        if (employSelect[i] === answer.id) {
+          empChoice = i + 1;
+        }
+      }
+      let newRole;
+      for (var i = 0; 1 < roleSelect.length; i++) {
+        if (roleSelect[i] === answer.role_id) {
+          newRole = i + 1;
+        }
+      }
+      let newMgr;
+      for (var i = 0; 1 < mgrSelect.length; i++) {
+        if (mgrSelect[i] === answer.manager_id) {
+          newMgr = i + 1;
+        }
+      }
+      let isMgr;
+      if (answer.is_manager == false) {
+        isMgr = 0;
+      } else {
+        isMgr = 1;
+      }
+
+      const sql = `UPDATE employee SET first_name = '${answer.first_name}', last_name = '${answer.last_name}', role_id = '${newRole}', is_manager = '${isMgr}', manager_id = '${newMgr}' WHERE id = '${empChoice}'`;
       db.query(sql, (err, rows) => {
         if (err) {
           console.log(err);
